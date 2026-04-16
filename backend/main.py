c1b71629d1b8a87efe0cfbd971b6e6ccdb11107f
Zed Rafhael Saranillo <saranillozed55@gmail.com>
2026-04-15 18:05:25 -0700

    Backend and Frontend work together.

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", ],
+    allow_origins=["http://localhost:3000", "http://localhost:5174", ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
